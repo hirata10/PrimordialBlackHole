5d597f91bf6173ea44b0e49d21e6376d3226f05d
koivuemily <84404151+koivuemily@users.noreply.github.com>
2023-10-20 14:45:42 -0400

    Add files via upload

---
+++ Paper2FinalCode/MakeIFuncs/M1e21-l1/MasterCopies/MinusPlus/GeneratorInUpUp.pl
@@ -0,0 +1,25 @@
+$N = 100;
+$omega = 5;
+$total = 2000 - $omega*100;
+$j_max = $total/$N;
+$l=1;
+## i gives h indexing 
+
+
+for $j (0..$j_max-1){
+    print "round $j/$j_max: "; system "date";
+    for $i ($j*$N..(($j+1)*$N)-1) {
+            print "fork $i: "; system "date";
+            my $pid = fork();
+            if (not $pid) {
+                    system("python trialParallelInUpUp.py $i InUpUp/trialParallelresulte$i.fits InUpUp/trialParallelresulto$i.fits $omega $l");
+                    exit();
+            }
+    }
+    for $i (0..$N-1) {
+            wait();
+            print "waited $i/$N: "; system "date";
+    }
+}
+##for $i (0..$N-1) {system("cat trialParallelresulte$i.fits > testingParallele.fits");}
+##for $i (0..$N-1) {system("cat trialParallelresulto$i.fits > testingParallelo.fits");}
